3a8df5c49e19d6de5de747bbeb8fecf1fcfae65d
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2025-01-06 22:59:37 +0900

    renamed files

--- LocaBet/App/StubetApp.swift
+++ LocaBet/App/StubetApp.swift
@@ -18,7 +18,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     
 
 @main
-struct StubetApp: App {
+struct LocaBetApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
